5d847fee0cc27ccd44ee847cd59662b7ffb4c463
Alp-i <alperenbaydur00@gmail.com>
2025-05-16 11:16:08 +0300

    Initial commit

---
+++ src/main/java/com/example/jenkinskubernetes/JenkinsKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinskubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsKubernetesApplication.class, args);
+    }
+
+}
